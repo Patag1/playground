eb6e90cf0aa7d3540793ee0c4e3ce607dfe343c2
Augusto <augustosasso97@gmail.com>
2022-10-20 01:24:41 -0300

    lecture 4 not completed

---
+++ CS50 2022/lecture4/addresses.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = 50;
+    int *p = &n;
+    // printf("%p\n", &n); both ways are valid
+    printf("%p\n", p); // shows address
+    printf("%i\n", *p); // shows to whatever the pointer points at (50)
+
+    // CS50 string
+    // string s = "HI!";
+
+    // C pointer to string (array of chars)
+    char *s = "HI!";
+    printf("%p\n", &s[0]);  // prints address of a char
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    
+    printf("%c\n", *s);     // prints whatever char is at *s
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+}
